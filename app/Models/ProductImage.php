838f22067a846346265607558d5fbc5e509692b1
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-12-09 22:43:14 +0600

    Product_Image Database creation

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     use HasFactory;
+    protected $fillable=['id', 'product_id', 'image'];
+
+    public function Product(){
+        return $this->belongsTo(Product::class,'product_id');
+    }
 }
--- database/migrations/2023_12_09_162607_create_product_images_table.php
+++ database/migrations/2023_12_09_162607_create_product_images_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('image');
             $table->timestamps();
         });
     }
